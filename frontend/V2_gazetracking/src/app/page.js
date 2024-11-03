cbac89cdb9c799d801c5e9628296c77cea318618
oceantran27 <daiduong3a1@gmail.com>
2024-11-03 23:29:09 +0700

    fix calibration

--- frontend/V2_gazetracking/src/app/page.js
+++ frontend/V2_gazetracking/src/app/page.js
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import WebGazer from "webgazer";
+import localforage from "localforage";
 
 export default function Home() {
   const [isCalibrating, setCalibrating] = useState(true);
@@ -11,52 +12,57 @@ export default function Home() {
 
   useEffect(() => {
     const isCalibrationDone = localStorage.getItem("webgazerCalibrationDone");
-
-    
+    console.log(isCalibrationDone);
     if (!isCalibrationDone) {
       //WebGazer.saveDataAcrossSessions = true;
-    setCalibrationPoints([
-      { x: window.innerWidth * 0.5, y: window.innerHeight * 0.5 },
-      { x: window.innerWidth * 0.1, y: window.innerHeight * 0.1 },
-      { x: window.innerWidth * 0.9, y: window.innerHeight * 0.1 },
-      { x: window.innerWidth * 0.1, y: window.innerHeight * 0.9 },
-      { x: window.innerWidth * 0.9, y: window.innerHeight * 0.9 },
-      { x: window.innerWidth * 0.5, y: window.innerHeight * 0.1 },
-      { x: window.innerWidth * 0.5, y: window.innerHeight * 0.9 },
-      { x: window.innerWidth * 0.1, y: window.innerHeight * 0.5 },
-      { x: window.innerWidth * 0.9, y: window.innerHeight * 0.5 },
-    ]);
-    console.log("WebGazer loaded");
+      setCalibrationPoints([
+        { x: window.innerWidth * 0, y: window.innerHeight * 0.83 },
+        { x: window.innerWidth * 0, y: window.innerHeight * 0.46 },
+        { x: window.innerWidth * 0, y: window.innerHeight * 0.1 },
+        { x: window.innerWidth * 0.5, y: window.innerHeight * 0 },
+        { x: window.innerWidth * 0.96, y: window.innerHeight * 0.1 },
+        { x: window.innerWidth * 0.96, y: window.innerHeight * 0.46 },
+        { x: window.innerWidth * 0.96, y: window.innerHeight * 0.83 },
+        { x: window.innerWidth * 0.5, y: window.innerHeight * 0.93 },
+        { x: window.innerWidth * 0.5, y: window.innerHeight * 0.5 },
+      ]);
+      console.log("WebGazer loaded");
 
-    //test
-    const gazeListener = (data) => {
-      if (data) {
-        console.log(data);
+      //test
+      const gazeListener = (data) => {
+        if (data) {
+          console.log(data);
+        } else console.log("no data?");
+      };
+
+      WebGazer.setRegression("ridge")
+        .setGazeListener((data, clock) => {}) //test
+        .saveDataAcrossSessions(true)
+        .applyKalmanFilter(true)
+        .showPredictionPoints(true)
+        .showVideoPreview(false)
+        .begin();
+
+      window.saveDataAcrossSessions = true;
+    } else {
+      async function getCalibrationData() {
+        try {
+          const data = await localforage.getItem("webgazerGlobalData");
+          console.log("Calibration data:", data);
+        } catch (error) {
+          console.error("Error retrieving data:", error);
+        }
       }
-      else console.log("no data?")
-    };
-
-
-    WebGazer.setRegression("ridge")
-      .setGazeListener((data, clock) => {}) //test
-      .saveDataAcrossSessions(true)
-      .applyKalmanFilter(true)
-      .showPredictionPoints(true)
-      .showVideoPreview(false)
-      .begin();
-
-    window.saveDataAcrossSessions = true;
-
-  } else {
-    console.log("Calibration already completed. Skipping calibration steps.");
-    WebGazer.setRegression("ridge")
-      .setGazeListener((data, clock) => {})
-      .saveDataAcrossSessions(true)
-      .applyKalmanFilter(true)
-      .showPredictionPoints(true)
-      .showVideoPreview(false)
-      .begin();
-  }
+      getCalibrationData();
+      console.log("Calibration already completed. Skipping calibration steps.");
+      WebGazer.setRegression("ridge")
+        .setGazeListener((data, clock) => {})
+        .saveDataAcrossSessions(true)
+        .applyKalmanFilter(true)
+        .showPredictionPoints(true)
+        .showVideoPreview(false)
+        .begin();
+    }
 
     return () => {
       WebGazer.end();
@@ -65,12 +71,6 @@ export default function Home() {
 
   const handleCalibrationClick = (pointIndex) => {
     if (clickCounts[pointIndex] < 5) {
-      // WebGazer.recordScreenPosition(
-      //   calibrationPoints[pointIndex].x,
-      //   calibrationPoints[pointIndex].y,
-      //   "click"
-      // );
-      // WebGazer.params.storingPoints = true;
       setClickCounts((prevCounts) => {
         const newCounts = [...prevCounts];
         newCounts[pointIndex] += 1;
@@ -82,18 +82,11 @@ export default function Home() {
       console.log("Calibration completed");
       setCalibrating(false);
       localStorage.setItem("webgazerCalibrationDone", "true"); // Save the calibration completion flag
-      calculateAccuracy();
     } else {
       setCalibrationStep(
         (prevStep) => (prevStep + 1) % calibrationPoints.length
       );
     }
-  };
-
-  const calculateAccuracy = () => {
-    const past50Points = WebGazer.getStoredPoints();
-    const accuracy = calculatePrecision(past50Points);
-    alert(`Độ chính xác của bạn là ${accuracy}%`);
   };
 
   if (!isCalibrating) {
@@ -105,9 +98,13 @@ export default function Home() {
   }
 
   return (
-    <div style={{ textAlign: "center", marginTop: "20%" }}>
-      <h1>Calibration</h1>
-      <h2>Hãy click vào điểm hiển thị ít nhất 5 lần mỗi điểm</h2>
+    <div>
+      <div style={{ textAlign: "center", marginTop: "15%" }}>
+        <h1 style={{ color: "red" }}>CALIBRATION</h1>
+        <h2>Hãy click vào điểm mục tiêu (màu xanh) ít nhất 5 lần mỗi điểm</h2>
+        <h2>Chỉ click khi chấm đỏ nằm gần nhất với điểm mục tiêu</h2>
+      </div>
+
       {calibrationPoints.map((point, index) => (
         <button
           key={index}
@@ -117,7 +114,7 @@ export default function Home() {
             top: point.y,
             width: 50,
             height: 50,
-            backgroundColor: clickCounts[index] < 5 ? "red" : "yellow",
+            backgroundColor: clickCounts[index] < 5 ? "Blue" : "yellow",
             border: "none",
             borderRadius: "50%",
             cursor: "pointer",
@@ -127,119 +124,6 @@ export default function Home() {
           {index + 1}
         </button>
       ))}
-      {/* {isPopupVisible && <Popup onClose={() => setPopupVisible(false)} />} */}
     </div>
   );
 }
-
-function calculatePrecision(points) {
-  const windowHeight = window.innerHeight;
-  const windowWidth = window.innerWidth;
-  const centerX = windowWidth / 2;
-  const centerY = windowHeight / 2;
-
-  let totalPrecision = 0;
-  points.forEach(({ x, y }) => {
-    const distance = Math.sqrt(
-      Math.pow(centerX - x, 2) + Math.pow(centerY - y, 2)
-    );
-    const precision = Math.max(0, 100 - (distance / (windowHeight / 2)) * 100);
-    totalPrecision += precision;
-  });
-
-  return Math.round(totalPrecision / points.length);
-}
-
-// "use client";
-
-// import { useEffect, useState } from "react";
-// import WebGazer from "webgazer";
-// import Popup from "./components/Popup";
-
-// export default function Home() {
-//   const [isPopupVisible, setPopupVisible] = useState(true);
-//   const [isCalibrating, setCalibrating] = useState(true);
-//   const [calibrationStep, setCalibrationStep] = useState(0);
-//   const [calibrationPoints, setCalibrationPoints] = useState([]);
-//   const [clickCounts, setClickCounts] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
-
-//   useEffect(() => {
-//     if (typeof window !== "undefined") {
-//       setCalibrationPoints([
-//         { x: window.innerWidth * 0, y: window.innerHeight * 0.75 },
-//         { x: window.innerWidth * 0, y: window.innerHeight * 0.25 },
-//         { x: window.innerWidth * 0.25, y: window.innerHeight * 0 },
-//         { x: window.innerWidth * 0.75, y: window.innerHeight * 0 },
-//         { x: window.innerWidth * 0.95, y: window.innerHeight * 0.25 },
-//         { x: window.innerWidth * 0.95, y: window.innerHeight * 0.75 },
-//         { x: window.innerWidth * 0.75, y: window.innerHeight * 0.93 },
-//         { x: window.innerWidth * 0.25, y: window.innerHeight * 0.93 },
-//       ]);
-
-//       WebGazer.setGazeListener((data) => {
-//         if (data && !isCalibrating) {
-//           const { x, y } = data;
-//           if (y > window.innerHeight * 0.8) {
-//             setPopupVisible(false);
-//           }
-//         }
-//       })
-//         .saveDataAcrossSessions(true) // Enable saving data for future sessions
-//         .applyKalmanFilter(true) // Apply Kalman Filter for smoother tracking
-//         .showVideo(false)
-//         .begin();
-
-//       // Run calibration process
-//       // runCalibrationPoints();
-
-//       return () => {
-//         WebGazer.end();
-//       };
-//     }
-//   }, [isCalibrating]);
-
-//   const handleCalibrationClick = (point) => {
-//     if (clickCounts[point] < 5) {
-//       WebGazer.recordScreenPosition(
-//         calibrationPoints[point].x,
-//         calibrationPoints[point].y,
-//         "click"
-//       );
-//       setClickCounts((prevCounts) => ({
-//         ...prevCounts,
-//         [point]: prevCounts[point] + 1,
-//       }));
-//     }
-
-//     if (Object.values(clickCounts).every((count) => count >= 5)) {
-//       setCalibrating(false);
-//     }
-//   };
-
-//   return (
-//     <div style={{ textAlign: "center", marginTop: "20%" }}>
-//       <h1>Click vào mỗi điểm ít nhất 5 lần</h1>
-//       <h2>*Lưu ý: Không chớp mắt trong quá trình click</h2>
-//       {calibrationPoints.map((point, index) => (
-//         <button
-//           key={index}
-//           style={{
-//             position: "absolute",
-//             left: point.x,
-//             top: point.y,
-//             width: 50,
-//             height: 50,
-//             backgroundColor: clickCounts[index] < 5 ? "red" : "yellow",
-//             border: "none",
-//             borderRadius: "50%",
-//             cursor: "pointer",
-//           }}
-//           onClick={() => handleCalibrationClick(index)}
-//         >
-//           {index + 1}
-//         </button>
-//       ))}
-//       {/* {isPopupVisible && <Popup onClose={() => setPopupVisible(false)} />} */}
-//     </div>
-//   );
-// }"
