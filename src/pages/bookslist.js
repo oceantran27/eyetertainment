1a5460beb75ef5bd5f14b0bb4a5f5fafd0449d3c
oceantran27 <daiduong3a1@gmail.com>
2024-11-26 16:31:21 +0700

    Fix dependencies conflict

--- src/pages/bookslist.js
+++ src/pages/bookslist.js
@@ -1,12 +1,10 @@
-import { pauseWebGazer, resumeWebGazer } from "@/components/webGazerWrapper";
 import { useEffect, useState } from "react";
-import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 import axios from "axios";
 import Layout from "@/components/Layout";
 
 export default function BooksList() {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const booksPerPage = 4; 
+  const booksPerPage = 4;
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
@@ -17,12 +15,15 @@ export default function BooksList() {
 
   const fetchBooks = async () => {
     try {
-      const response = await axios.get('https://otruyenapi.com/v1/api/danh-sach/hoan-thanh?page=1', {
-        headers: { 'Accept': 'application/json' }
-      });
-      setBooks(response.data?.data?.items || []); 
+      const response = await axios.get(
+        "https://otruyenapi.com/v1/api/danh-sach/hoan-thanh?page=1",
+        {
+          headers: { Accept: "application/json" },
+        }
+      );
+      setBooks(response.data?.data?.items || []);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
 
@@ -39,7 +40,12 @@ export default function BooksList() {
   };
 
   return (
-    <Layout books={books} currentIndex={currentIndex} goToNext={goToNext} goToPrevious={goToPrevious}>
+    <Layout
+      books={books}
+      currentIndex={currentIndex}
+      goToNext={goToNext}
+      goToPrevious={goToPrevious}
+    >
       {/* Nội dung sẽ được hiển thị ở Layout */}
     </Layout>
   );
