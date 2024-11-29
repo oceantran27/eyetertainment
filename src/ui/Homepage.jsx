e576c15c3d5f9fc60b3b302cf54171317159921a
Trang Vuong <trangvuong2810@gmail.com>
2024-11-29 23:37:02 +0700

    add icon and hero-image

--- src/ui/Homepage.jsx
+++ src/ui/Homepage.jsx
@@ -9,12 +9,12 @@ export default function HomePage() {
             {/* <div className="justify-center gap-6 rounded-lg bg-[#1a1b20] px-6 py-10 md:px-20 w-full h-full"> */}
                 <div className="flex-auto flex flex-col md:flex-row md:gap-36 justify-center items-center text-center  p-4">
                     <div className='flex flex-col items-center justify-center'>
-                        <h1 className="text-3xl md:text-5xl font-bold mb-4 text-[#e2e2e9]">Your Slogan Here</h1>
-                        <p className="text-lg text-[#adc6ff]">This is a description of your brand or product.</p>
+                        <h1 className="text-3xl md:text-5xl font-bold mb-4 text-[#e2e2e9]">Eyetertainer</h1>
+                        <p className="text-lg text-[#adc6ff]">See the fun, feel the joy – Control with your eyes, enjoy with your heart.</p>
                     </div>
                     <div className="mb-6">
                     {/* Logo */}
-                        <Image src="/images/quiz.png" alt="Logo" width={350} height={350} />
+                        <Image src="/images/hero-image.png" alt="hero image" width={350} height={350} />
                     </div>
                 </div>
 
