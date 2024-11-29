31bb274161eefc21a3f8e75ebb53a1612d0de92a
Trang Vuong <trangvuong2810@gmail.com>
2024-11-29 22:29:11 +0700

    fix minor ui elements

--- src/ui/Login.jsx
+++ src/ui/Login.jsx
@@ -215,120 +215,122 @@ function Login() {
   }
 
   return (
-    <div className="h-full flex flex-col items-center justify-center gap-[24px] max-w-[720px] mx-auto">
-      {!localUserStream && !modelsLoaded && (
-        <h2 className="text-center text-3xl font-extrabold tracking-tight text-[#adc6ff] pt-10 sm:text-4xl">
-          <span className="block">
-            You're Attempting to Log In With Your Face
-          </span>
-          <span className="block text-[#adc6ff] mt-2">Loading Models...</span>
-        </h2>
-      )}
-      {!localUserStream && modelsLoaded && (
-        <h2 className="text-center text-3xl font-extrabold tracking-tight text-[#adc6ff] pt-10 sm:text-4xl">
-          <span className="block text-[#adc6ff] mt-2">
-            Please Recognize Your Face to Completely Log In
-          </span>
-        </h2>
-      )}
-      {localUserStream && loginResult === "SUCCESS" && (
-        <h2 className="text-center text-3xl font-extrabold tracking-tight text-[#adc6ff] sm:text-4xl">
-          <span className="block text-[#adc6ff] mt-2">
-            We've successfully recognize your face!
-          </span>
-          <span className="block text-[#adc6ff] mt-2">
-            Please stay {counter} more seconds...
-          </span>
-        </h2>
-      )}
-      {localUserStream && loginResult === "FAILED" && (
-        <h2 className="text-center text-3xl font-extrabold tracking-tight text-[#ffb4ab] sm:text-4xl">
-          <span className="block mt-[56px]">
-            Oops! We did not recognize your face
-          </span>
-        </h2>
-      )}
-      {localUserStream && !faceApiLoaded && loginResult === "PENDING" && (
-        <h2 className="text-center text-3xl font-extrabold tracking-tight text-[#adc6ff] sm:text-4xl">
-          <span className="block mt-[56px]">Scanning Face...</span>
-        </h2>
-      )}
-      <div className="w-full">
-        <div className="relative flex flex-col items-center p-[10px]">
-          <video
-            muted
-            autoPlay
-            ref={videoRef}
-            height={videoHeight}
-            width={videoWidth}
-            onPlay={scanFace}
-            style={{
-              objectFit: "fill",
-              height: "360px",
-              borderRadius: "10px",
-              display: localUserStream ? "block" : "none",
-            }}
-          />
-          <canvas
-            ref={canvasRef}
-            style={{
-              position: "absolute",
-              display: localUserStream ? "block" : "none",
-            }}
-          />
-        </div>
-        {!localUserStream && (
-          <>
-            {modelsLoaded ? (
-              <>
-                <img
-                  alt="loading models"
-                  src="/images/auth-face.png"
-                  className="cursor-pointer my-8 mx-auto object-cover h-[300px]"
-                />
-                <button
-                  onClick={getLocalUserVideo}
-                  type="button"
-                  className="flex justify-center items-center w-full py-2.5 px-5 mr-2 text-lg text-[#112f60] bg-[#adc6ff] hover:bg-[#2b4678] hover:text-[#d8e2ff] rounded-lg "
-                >
-                  Scan my face
-                </button>
-              </>
-            ) : (
-              <>
-                <img
-                  alt="loading models"
-                  src="/images/auth-idle.png"
-                  className="cursor-pointer my-8 mx-auto object-cover h-[300px]"
-                />
-                <button
-                  disabled
-                  type="button"
-                  className="cursor-not-allowed flex justify-center items-center w-full py-2.5 px-5 text-lg text-[#112f60] bg-[#adc6ff] rounded-lg hover:bg-[#2b4678] hover:text-[#d8e2ff]"
-                >
-                  <svg
-                    aria-hidden="true"
-                    role="status"
-                    className="inline mr-2 w-4 h-4 text-[#112f60] bg-[#adc6ff] animate-spin"
-                    viewBox="0 0 100 101"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                      fill="currentColor"
-                    />
-                    <path
-                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                      fill="#1C64F2"
-                    />
-                  </svg>
-                  Please wait while models were loading...
-                </button>
-              </>
-            )}
-          </>
+    <div className="max-w-scrren overflow-y-auto max-h-screen">
+      <div className=" overflow-y-auto flex flex-col items-center justify-center max-w-screen mx-auto">
+        {!localUserStream && !modelsLoaded && (
+          <h2 className="max-w-[720px] text-center text-3xl font-extrabold tracking-tight text-[#adc6ff] pt-10 sm:text-4xl">
+            <span className="block">
+              You're Attempting to Log In With Your Face
+            </span>
+            <span className="block text-[#adc6ff] mt-2">Loading Models...</span>
+          </h2>
         )}
+        {!localUserStream && modelsLoaded && (
+          <h2 className="max-w-[720px] text-center text-3xl font-extrabold tracking-tight text-[#adc6ff] pt-10 sm:text-4xl">
+            <span className="block text-[#adc6ff] mt-2">
+              Please Recognize Your Face to Completely Log In
+            </span>
+          </h2>
+        )}
+        {localUserStream && loginResult === "SUCCESS" && (
+          <h2 className="max-w-[720px] text-center text-3xl font-extrabold tracking-tight text-[#adc6ff] sm:text-4xl">
+            <span className="block text-[#adc6ff] mt-2">
+              We've successfully recognize your face!
+            </span>
+            <span className="block text-[#adc6ff] mt-2">
+              Please stay {counter} more seconds...
+            </span>
+          </h2>
+        )}
+        {localUserStream && loginResult === "FAILED" && (
+          <h2 className="max-w-[720px] text-center text-3xl font-extrabold tracking-tight text-[#ffb4ab] sm:text-4xl">
+            <span className="block mt-[56px]">
+              Oops! We did not recognize your face
+            </span>
+          </h2>
+        )}
+        {localUserStream && !faceApiLoaded && loginResult === "PENDING" && (
+          <h2 className="max-w-[720px] text-center text-3xl font-extrabold tracking-tight text-[#adc6ff] sm:text-4xl">
+            <span className="block mt-[56px]">Scanning Face...</span>
+          </h2>
+        )}
+        <div className="max-w-[720px] my-5">
+          <div className="relative flex flex-col items-center p-[10px]">
+            <video
+              muted
+              autoPlay
+              ref={videoRef}
+              height={videoHeight}
+              width={videoWidth}
+              onPlay={scanFace}
+              style={{
+                objectFit: "fill",
+                height: "360px",
+                borderRadius: "10px",
+                display: localUserStream ? "block" : "none",
+              }}
+            />
+            <canvas
+              ref={canvasRef}
+              style={{
+                position: "absolute",
+                display: localUserStream ? "block" : "none",
+              }}
+            />
+          </div>
+          {!localUserStream && (
+            <>
+              {modelsLoaded ? (
+                <>
+                  <img
+                    alt="loading models"
+                    src="/images/auth-face.png"
+                    className="cursor-pointer my-8 mx-auto object-cover h-[300px]"
+                  />
+                  <button
+                    onClick={getLocalUserVideo}
+                    type="button"
+                    className="flex justify-center items-center w-full py-2.5 px-5 mr-2 text-lg text-[#112f60] bg-[#adc6ff] hover:bg-[#2b4678] hover:text-[#d8e2ff] rounded-lg "
+                  >
+                    Scan my face
+                  </button>
+                </>
+              ) : (
+                <>
+                  <img
+                    alt="loading models"
+                    src="/images/auth-idle.png"
+                    className="cursor-pointer my-8 mx-auto object-cover h-[300px]"
+                  />
+                  <button
+                    disabled
+                    type="button"
+                    className="cursor-not-allowed flex justify-center items-center w-full py-2.5 px-5 text-lg text-[#112f60] bg-[#adc6ff] rounded-lg hover:bg-[#2b4678] hover:text-[#d8e2ff]"
+                  >
+                    <svg
+                      aria-hidden="true"
+                      role="status"
+                      className="inline mr-2 w-4 h-4 text-[#112f60] bg-[#adc6ff] animate-spin"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="#1C64F2"
+                      />
+                    </svg>
+                    Please wait while models were loading...
+                  </button>
+                </>
+              )}
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
