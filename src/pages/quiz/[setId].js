bd549cc67ac51db2d43515b5228714e6e03e7784
oceantran27 <daiduong3a1@gmail.com>
2024-12-01 15:36:46 +0700

    Add gaze-button

--- src/pages/quiz/[setId].js
+++ src/pages/quiz/[setId].js
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import GazeButton from "@/components/gazeButton";
 
 export const metadata = {
-  title: 'Play | Quiz | Eyetertainment',
+  title: "Play | Quiz | Eyetertainment",
 };
 
 const QuizDetailPage = () => {
@@ -32,14 +33,14 @@ const QuizDetailPage = () => {
 
   const handleAnswerClick = (answer) => {
     if (answer === questions[currentQuestionIndex].correct_answer) {
-      setScore(prevScore => prevScore + 1); // Tinh diem
+      setScore((prevScore) => prevScore + 1); // Tinh diem
     }
 
     setSelectedAnswer(answer);
     setTimeout(() => {
       setSelectedAnswer(null);
-      setCurrentQuestionIndex(prevIndex => prevIndex + 1); // chuyen cau hoi
-    }, 0); 
+      setCurrentQuestionIndex((prevIndex) => prevIndex + 1); // chuyen cau hoi
+    }, 0);
   };
 
   // kiem tra xem het cau hoi chua
@@ -47,11 +48,16 @@ const QuizDetailPage = () => {
     return (
       <div className="flex justify-center items-center h-screen">
         <div className="bg-[#1e1f25] w-[1000px] h-[600px] p-6 rounded-xl flex flex-col justify-between">
-          <h1 className="text-2xl font-bold mb-8 text-[#c4c6d0] text-center mt-10">Quiz Completed</h1>
-          <p className="text-3xl font-semibold text-lg mb-6 text-[#c4c6d0] text-center">Your score: {score} / {questions.length}</p>
-          <button 
-            onClick={() => router.push('/quiz')} 
-            className="font-bold w-[300px] h-[100px] py-3 rounded-lg text-xl text-[#402843] bg-[#debcdf] hover:bg-[#583e5b] hover:text-[#fcd7fb] transition mb-10 mx-auto">
+          <h1 className="text-2xl font-bold mb-8 text-[#c4c6d0] text-center mt-10">
+            Quiz Completed
+          </h1>
+          <p className="text-3xl font-semibold text-lg mb-6 text-[#c4c6d0] text-center">
+            Your score: {score} / {questions.length}
+          </p>
+          <button
+            onClick={() => router.push("/quiz")}
+            className="font-bold w-[300px] h-[100px] py-3 rounded-lg text-xl text-[#402843] bg-[#debcdf] hover:bg-[#583e5b] hover:text-[#fcd7fb] transition mb-10 mx-auto"
+          >
             Try Another Quiz
           </button>
         </div>
@@ -63,30 +69,34 @@ const QuizDetailPage = () => {
 
   // shuffle answers
   const allAnswers = [
-    ...JSON.parse(currentQuestion.incorrect_answers), 
-    currentQuestion.correct_answer
+    ...JSON.parse(currentQuestion.incorrect_answers),
+    currentQuestion.correct_answer,
   ];
   const shuffledAnswers = allAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="bg-[#1e1f25] w-[1000px] h-[600px] p-6 rounded-xl flex flex-col justify-between">
-        <h1 className="text-2xl font-bold mb-8 text-[#c4c6d0] text-center mt-10">Question {currentQuestionIndex + 1}: {currentQuestion.question}</h1>
+        <h1 className="text-2xl font-bold mb-8 text-[#c4c6d0] text-center mt-10">
+          Question {currentQuestionIndex + 1}: {currentQuestion.question}
+        </h1>
 
         <div className="grid grid-cols-2 gap-10 justify-items-center">
           {shuffledAnswers.map((answer, index) => (
-            <button
+            <GazeButton
               key={index}
               onClick={() => handleAnswerClick(answer)}
               className={`w-[300px] h-[100px] py-4 rounded-lg text-[#402843] font-semibold bg-[#debcdf] hover:bg-[#583e5b] hover:text-[#fcd7fb] transition 
-                ${selectedAnswer === answer ? 'bg-[#6495ed]' : ''} text-2xl`}
+                ${selectedAnswer === answer ? "bg-[#6495ed]" : ""} text-2xl`}
             >
               {answer}
-            </button>
+            </GazeButton>
           ))}
         </div>
 
-        <div className="mt-6 text-[#c4c6d0] text-lg font-bold text-center">Score: {score}</div>
+        <div className="mt-6 text-[#c4c6d0] text-lg font-bold text-center">
+          Score: {score}
+        </div>
       </div>
     </div>
   );
@@ -96,7 +106,7 @@ export async function getServerSideProps(context) {
   const { setId } = context.params;
   return {
     props: {
-      setId, 
+      setId,
     },
   };
 }
--- src/pages/testGaze.js
+++ src/pages/testGaze.js
@@ -1,20 +1,13 @@
 import React from "react";
 import { useGazeData } from "@/hooks/GazeContext";
+import HoverButton from "@/components/tmp";
 
 function DisplayGaze() {
   const gazeData = useGazeData();
 
   return (
     <div>
-      <h1>Gaze Data</h1>
-      {gazeData ? (
-        <ul>
-          <li>GazeX: {gazeData.GazeX}</li>
-          <li>GazeY: {gazeData.GazeY}</li>
-        </ul>
-      ) : (
-        <p>No gaze data available</p>
-      )}
+      <HoverButton />
     </div>
   );
 }
